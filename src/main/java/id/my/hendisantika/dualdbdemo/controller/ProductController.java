45448a9943181c5e411864bb67a1dbc064a9453c
hendisantika <hendisantika@yahoo.co.id>
2025-11-28 18:23:02 +0700

    feat: Add ProductController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/dualdbdemo/controller/ProductController.java
@@ -0,0 +1,45 @@
+package id.my.hendisantika.dualdbdemo.controller;
+
+import id.my.hendisantika.dualdbdemo.dto.ProductRequest;
+import id.my.hendisantika.dualdbdemo.dto.ProductResponse;
+import id.my.hendisantika.dualdbdemo.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : dual-db-demo
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 28/11/25
+ * Time: 17.30
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/all")
+    public Flux<ProductResponse> getAllProductsFromBothDatabases() {
+        return productService.getAllProductsFromBothDatabases();
+    }
+
+    @PostMapping("/sync")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Void> syncProductToBothDatabases(@RequestBody ProductRequest request) {
+        return productService.syncProductToBothDatabases(request);
+    }
+}
