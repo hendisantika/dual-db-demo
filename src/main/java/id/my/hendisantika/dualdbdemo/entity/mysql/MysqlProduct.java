b2e15d78e028b02bcbdec03e261a663eac57fd3a
hendisantika <hendisantika@yahoo.co.id>
2025-11-28 18:21:53 +0700

    feat: Add MysqlProduct🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/dualdbdemo/entity/mysql/MysqlProduct.java
@@ -0,0 +1,52 @@
+package id.my.hendisantika.dualdbdemo.entity.mysql;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : dual-db-demo
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 28/11/25
+ * Time: 17.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("products")
+public class MysqlProduct {
+
+    @Id
+    private Long id;
+
+    @Column("name")
+    private String name;
+
+    @Column("description")
+    private String description;
+
+    @Column("price")
+    private BigDecimal price;
+
+    @Column("quantity")
+    private Integer quantity;
+
+    @Column("created_at")
+    private LocalDateTime createdAt;
+
+    @Column("updated_at")
+    private LocalDateTime updatedAt;
+}
